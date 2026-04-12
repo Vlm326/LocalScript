f336535a178d0e5b26590f4ec8a5261127dd9357
Frolov_K_F <st141840@student.spbu.ru>
2026-04-12 22:35:48 +0300

    Add extrac_validation_feedback func

--- llm-service/app/sandbox_client.py
+++ llm-service/app/sandbox_client.py
@@ -1,20 +1,21 @@
+import asyncio
 import os
-
 import httpx
-import uvicorn
+import json
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from config import SANDBOX_SERVICE_URL
 
 app = FastAPI(title="LLM Service", version="0.1.0")
 
-SANDBOX_URL = os.getenv("SANDBOX_SERVICE_URL", "http://0.0.0.0:6778")
+SANDBOX_URL = os.getenv(SANDBOX_SERVICE_URL, "http://0.0.0.0:6778")
 
 
 class ValidateRequest(BaseModel):
     code: str
     execute: bool = False
     timeout: int = 2
-
 
 @app.post("/validate")
 async def validate(req: ValidateRequest):
@@ -31,5 +32,26 @@ async def validate(req: ValidateRequest):
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+async def send_code_for_validation(code: str):
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(
+            f"{SANDBOX_URL}/validate",
+            json={"code": code, "execute": True, "timeout": 2}
+        )
+        resp.raise_for_status()
+        return resp.json()
+
+
+def extract_validation_feedback(responce):
+    responce_dict = json.loads(responce)
+    if responce_dict['status']['status'] == 'ok':
+        return True
+    else:
+        error = responce_dict['error_detail']
+        feedback = f'{error['kind']}: "{error['message']}" in code part: {error['snippet']}'
+        return feedback
+
+
 
 
