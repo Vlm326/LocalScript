c5dddc86ee866d3a53a6bf790b9a0c3c70468cf7
zak-p-y <st142062@student.spbu.ru>
2026-04-12 21:03:39 +0300

    Add asynchrony: change class GenerationPipeline

--- llm-service/app/pipeline.py
+++ llm-service/app/pipeline.py
@@ -26,35 +26,35 @@ class GenerationPipeline:
         self.client = OllamaClient(model_name, url = url)
         self.max_retries = max_retries
 
-    def _generate_plan(self, task: str, time: int) -> str:
+    async def _generate_plan(self, task: str, time: int) -> str:
         start_plan_time = time.perf_counter()
         messages = prompts.build_architect_messages(task)
-        result = self.client.send_request(messages, keep_alive=300)
+        result = await self.client.send_request(messages, keep_alive=300)
         end_plan_time = time.perf_counter()
         print("=" * 15, "\n", "PLAN_TIME: ", end_plan_time - start_plan_time)
         total_time += end_plan_time - start_plan_time
         return result or ""
 
-    def _generate_code(self, plan: str, task: str, rag_data: str = "", previous_code: str = '', critic_feedback: str = "") -> str:
+    async def _generate_code(self, plan: str, task: str, rag_data: str = "", previous_code: str = '', critic_feedback: str = "") -> str:
         start_code_time = time.perf_counter()
         messages = prompts.build_coder_messages(plan=plan, task=task, rag_data=rag_data, previous_code=previous_code, critic_feedback=critic_feedback)
-        result = self.client.send_request(messages, keep_alive=300)
+        result = await self.client.send_request(messages, keep_alive=300)
         end_code_time = time.perf_counter()
         print("=" * 15, "\n", "CODE_TIME: ", end_code_time - start_code_time)
         total_time += end_code_time - start_code_time
         return result or ""
     
 
-    def _critique_code(self, code: str) -> str:
+    async def _critique_code(self, code: str) -> str:
         start_feedback_time = time.perf_counter()
         messages = prompts.build_critic_messages(code)
-        result = self.client.send_request(messages, keep_alive=300)
+        result = await self.client.send_request(messages, keep_alive=300)
         end_feedback_time = time.perf_counter()
         print("=" * 15, "\n", "FEEDBACK_TIME: ", end_feedback_time - start_feedback_time)
         total_time += end_feedback_time - start_feedback_time
         return result or ""
  
-    def _fix_code(
+    async def _fix_code(
         self,
         plan: str,
         task: str,
@@ -69,7 +69,7 @@ class GenerationPipeline:
             previous_code=previous_code,
             critic_feedback=critic_feedback,
         )
-        result = self.client.send_request(messages, keep_alive=300)
+        result = await self.client.send_request(messages, keep_alive=300)
         return result or ""
 
     def _is_code_ok(self, feedback: str) -> bool:
