1a83f889633ed2334dbf41c4bfd5565b92c733c4
zak-p-y <st142062@student.spbu.ru>
2026-04-13 13:08:03 +0300

    Bug fix

--- llm-service/app/sandbox_client.py
+++ llm-service/app/sandbox_client.py
@@ -9,7 +9,7 @@ from config import SANDBOX_SERVICE_URL
 
 app = FastAPI(title="LLM Service", version="0.1.0")
 
-SANDBOX_URL = os.getenv(SANDBOX_SERVICE_URL, "http://0.0.0.0:6778")
+SANDBOX_URL = os.getenv("SANDBOX_SERVICE_URL", "http://0.0.0.0:6778")
 
 
 class ValidateRequest(BaseModel):
@@ -33,24 +33,27 @@ async def validate(req: ValidateRequest):
 async def health():
     return {"status": "ok"}
 
-async def send_code_for_validation(code: str):
+async def send_code_for_validation(code: str):     
     async with httpx.AsyncClient() as client:
         resp = await client.post(
             f"{SANDBOX_URL}/pipeline",
             json={"code": code, "execute": True, "timeout": 2}
         )
         resp.raise_for_status()
-        return resp.json()
+        return resp.json()               # Почему возвращаем сырой json на rust-service?
 
 
 def extract_validation_feedback(responce):
     responce_dict = json.loads(responce)
-    if responce_dict['status']['status'] == 'ok':
+    if responce_dict['status'] == 'ok':
         return True
     else:
         error = responce_dict['error_detail']
-        feedback = f'{error["kind"]}: "{error["message"]}" in code part: {error["snippet"]}'
-        return feedback
+        if error:
+            return f"""{error["kind"]}: {error["message"]} in code part: {error["snippet"]}"""
+        print("Error is Null")
+        return ""
+
 
 
 
