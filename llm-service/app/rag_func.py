3b0469e150392ceb9056e864e0915964dc07ed4c
V1eNen <22neon@bk.ru>
2026-04-14 21:26:53 +0300

    fixed rag_func.py

--- llm-service/app/rag_func.py
+++ llm-service/app/rag_func.py
@@ -1,26 +1,34 @@
-from qdrant_client import QdrantClient
+import httpx
+from qdrant_client import AsyncQdrantClient
 from config import QDRANT_URL
-import requests
 
-def get_emb_from_prompt(prompt, ip):
-    response = requests.post(
+async def connect_qdrant_client():
+    return AsyncQdrantClient(url=QDRANT_URL)
+
+async def get_emb_from_prompt(prompt: str, ip: str, http_client: httpx.AsyncClient):
+    response = await http_client.post(
         url=ip,
-        json={
-            "input": prompt,
-            "model": "bge-m3",
-        },
+        json={"input": prompt, "model": "bge-m3"},
     )
+    response.raise_for_status()
     return response.json()["data"][0]["embedding"]
 
-def get_from_rag(client, query, limit):
-    '''Возвращает массив: [айди, текст документа]'''
-    sim_points = client.query_points(collection_name = 'lua_patterns', query=query, limit=limit)
+async def get_from_rag(qdrant_client: AsyncQdrantClient, query_vector: list[float], limit: int = 1):
+    sim_points = await qdrant_client.search(
+        collection_name="lua_patterns",
+        query_vector=query_vector,
+        limit=limit,
+    )
+
+    if not sim_points:
+        return [None, ""]
+
     point = sim_points[0]
-    text_of_doc = f'''
-                    Конструкция: {point.payload['description']}
-                    Проверить: {point.payload['validation_checklist']}
-                    '''
+    payload = point.payload or {}
+
+    text_of_doc = f"""
+                    Конструкция: {payload.get('description', 'N/A')}
+                    Проверить: {payload.get('validation_checklist', 'N/A')}
+                    """
     
-    return [point.payload['id', text_of_doc]]
-
-
+    return [payload.get('id'), text_of_doc]
